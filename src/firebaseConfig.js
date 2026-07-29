d17ded5577b1a28e8e8793f0b340de826e41e227
Santiago Barretto <Santiago Barretto>
2026-07-29 20:01:48 -0300

    fix firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDclVml6BewwOWG5PQrtKyi28mNFiHOR44",
   authDomain: "agua-correa-e8c6b.firebaseapp.com",
   projectId: "agua-correa-e8c6b",
@@ -15,7 +7,13 @@ const firebaseConfig = {
   appId: "1:734391391296:web:8d6b04b87b74181552fba5",
   measurementId: "G-07VXHTFX6S"
 };
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firestore = getFirestore(firebaseApp);
+
+// Nombre de la colección de Firestore donde se guardan los datos del negocio.
+// Si alguna vez querés usar el mismo proyecto de Firebase para otro reparto,
+// cambiá este nombre para que no se mezclen los datos.
+export const COLLECTION = "repartoAgua";
